10183ba29f7b5870a6798d6edcefe5e913a31345
srinivasyadav18 <srinivasyadav227@icloud.com>
2022-06-20 06:25:07 +0000

    Update addition.cpp

--- tests/unit/addition.cpp
+++ tests/unit/addition.cpp
@@ -3,17 +3,38 @@
 #include <vector>
 #include <numeric>
 
-int main()
+void seq()
+{
+    typedef svint32_t Vector __attribute__((arm_sve_vector_bits(SVE_LEN)));
+    Vector vec = svdup_s32(5);
+    std::cout << "addv : " << svaddv(svptrue_b32(), vec) << "\n";
+
+    for (int idx = 0; idx < 4; idx++)
+    {
+        auto index = svptrue_pat_b32(SV_VL1);
+        for (int i = 0; i < idx; i++)
+        {
+            index = svpnext_b32(svptrue_b32(), index);
+        }
+        std::cout << "idx : " << idx << " " <<  svlastb(index, vec) << "\n";
+    }
+}
+
+template <typename T>
+void test()
 {
     using namespace sve::experimental;
-    simd<float> x(5);
+    simd<T> x(5);
+    std::cout << "addv : " << x.addv() << "\n";
     std::cout << "xlen : " << x.size() << '\n';
     std::cout << "x : " << x << '\n';
-    std::cout << "x : " << x + 1 << '\n';
+    auto tmp = x + 1;
+    std::cout << "got x + 1 into tmp" << '\n';
+    std::cout << "x : " << tmp << '\n';
     std::cout << "x : " << x++ << '\n';
     std::cout << "x : " << ++x << "\n\n";
 
-    std::vector<float> data(x.size());
+    std::vector<T> data(x.size());
     std::iota(data.begin(), data.end(), 1);
     x.copy_from(data.data(), vector_aligned);
 
@@ -32,5 +53,16 @@ int main()
     std::cout << "x : " << x - 1 << '\n';
     std::cout << "x : " << x-- << '\n';
     std::cout << "x : " << --x << '\n';
+}
+
+int main()
+{
+    seq();
+    test<int>();
+    test<float>();
+    test<double>();
+    test<int8_t>();
+    test<int16_t>();
     return 0;
 }
+
