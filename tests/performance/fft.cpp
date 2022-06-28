0030addbb56c3289d3947aa676ff078d3362e1c9
srinivasyadav18 <srinivasyadav227@icloud.com>
2022-06-28 15:23:10 +0000

    Improve fft perf test

--- tests/performance/fft.cpp
+++ tests/performance/fft.cpp
@@ -8,19 +8,34 @@
 #include <vector>
 #include <iostream>
 #include <cassert>
-
-using namespace std;
-using namespace sve::experimental;
-using cd = complex<double>;
-const double PI = acos(-1);
+#include <fstream>
+#include <chrono>
 
 template <typename T>
-std::pair<T, T> complex_mul(T a, T b, T c, T d)
+inline void complex_mul(T a, T b, T c, T d, T& res_re, T& res_im)
 {
-    return {a*c - b*d, a*d + b*c};
+    res_re = a*c - b*d;
+    res_im = a*d + b*c;
 }
 
-void fft(vector<double>& re, vector<double>& im, bool invert) {
+template <typename T>
+std::vector<T> gen_signal(T freq)
+{
+    const T PI = acos(-1);
+    T sf = T(16) * freq;
+    T ts = 1/sf;
+    std::vector<T> ret(sf, 0);
+    int idx = 0;
+    for (T i = T(0); i < T(1); i += ts)
+    {
+        ret[idx++] = T(10) * sin(T(2.0) * PI * freq * i);
+    }
+    return ret;
+}
+
+template <typename T>
+void fft(std::vector<T>& re, std::vector<T>& im, bool invert) {
+    const T PI = acos(-1);
     int n = re.size();
 
     for (int i = 1, j = 0; i < n; i++) {
@@ -28,79 +43,50 @@ void fft(vector<double>& re, vector<double>& im, bool invert) {
         for (; j & bit; bit >>= 1)
             j ^= bit;
         j ^= bit;
-
         if (i < j)
         {
-            swap(re[i], re[j]);
-            swap(im[i], im[j]);
+            std::swap(re[i], re[j]);
+            std::swap(im[i], im[j]);
         }
     }
 
     for (int len = 2; len <= n; len <<= 1) {
-        double ang = 2 * PI / len * (invert ? -1 : 1);
-        // std::cout << "fft cos : " << cos(ang) << '\t' << "sin : " << sin(ang) << "\n";
-
-        // cd wlen(cos(ang), sin(ang));
-        double wlen_re = cos(ang);
-        double wlen_im = sin(ang);
+        T ang = 2 * PI / len * (invert ? -1 : 1);
+        T wlen_re = cos(ang);
+        T wlen_im = sin(ang);
         for (int i = 0; i < n; i += len) {
-            // cd w(1);
-
-            double w_re = 1;
-            double w_im = 0;
-
+            T w_re = 1;
+            T w_im = 0;
             for (int j = 0; j < len / 2; j++) {
-                // cd u = a[i+j], v = a[i+j+len/2] * w;
                 int u = i+j;
                 int v = i+j+len/2;
-
-                double u_re = re[u];
-                double u_im = im[u];
-                double v_re = re[v];
-                double v_im = im[v];
-                auto tmp = complex_mul(v_re, v_im, w_re, w_im);
-                v_re = tmp.first;
-                v_im = tmp.second;
-
-                // a[i+j] = u + v;
+                T u_re = re[u];
+                T u_im = im[u];
+                T v_re = re[v];
+                T v_im = im[v];
+                complex_mul(v_re, v_im, w_re, w_im, v_re, v_im);
                 re[u] = u_re + v_re;
                 im[u] = u_im + v_im;
-
-                // a[i+j+len/2] = u - v;
                 re[v] = u_re - v_re;
                 im[v] = u_im - v_im;
-
-                // std::cout << "[Debug fft bef : " 
-                //     << "len = " << len 
-                //     << " i = " << i 
-                //     << " j = " << j
-                //     << " w_re = " << w_re 
-                //     << " w_im = " << w_im 
-                //     << " ]\n";
-                // w *= wlen;
-                tmp = complex_mul(w_re, w_im, wlen_re, wlen_im);
-                w_re = tmp.first;
-                w_im = tmp.second;
-                // std::cout << "[Debug fft : " 
-                //         << "len = " << len 
-                //         << " i = " << i 
-                //         << " j = " << j
-                //         << " w_re = " << w_re 
-                //         << " w_im = " << w_im 
-                //         << " ]\n";
+                complex_mul(w_re, w_im, wlen_re, wlen_im, w_re, w_im);
             }
         }
     }
 
     if (invert) {
-        for (double & x : re)
+        for (T & x : re)
             x /= n;
-        for (double & x : im)
+        for (T & x : im)
             x /= n;
     }
 }
 
-void fft_simd(vector<double>& re, vector<double>& im, bool invert) {
+template <typename T>
+void fft_simd(std::vector<T>& re, std::vector<T>& im, bool invert) {
+    using simd_t = sve::experimental::simd<T>;
+    const T PI = acos(-1);
+
     int n = re.size();
 
     for (int i = 1, j = 0; i < n; i++) {
@@ -111,246 +97,166 @@ void fft_simd(vector<double>& re, vector<double>& im, bool invert) {
 
         if (i < j)
         {
-            swap(re[i], re[j]);
-            swap(im[i], im[j]);
+            std::swap(re[i], re[j]);
+            std::swap(im[i], im[j]);
         }
     }
 
     for (int len = 2; len <= n; len <<= 1) {
-        if (len < 2 * (int)simd<double>::size())
+        if (len < 2 * (int)simd_t::size())
         {
-            // std::cout << "into serial code len : " << len << '\n';
-            double ang = 2 * PI / len * (invert ? -1 : 1);
-            // std::cout << "fft seq cos : " << cos(ang) << '\t' << "sin : " << sin(ang) << "\n";
+            T ang = 2 * PI / len * (invert ? -1 : 1);
 
-            // cd wlen(cos(ang), sin(ang));
-            double wlen_re = cos(ang);
-            double wlen_im = sin(ang);
+            T wlen_re = cos(ang);
+            T wlen_im = sin(ang);
             for (int i = 0; i < n; i += len) {
-                // cd w(1);
 
-                double w_re = 1;
-                double w_im = 0;
+                T w_re = 1;
+                T w_im = 0;
 
                 for (int j = 0; j < len / 2; j++) {
-                    // cd u = a[i+j], v = a[i+j+len/2] * w;
                     int u = i+j;
                     int v = i+j+len/2;
 
-                    double u_re = re[u];
-                    double u_im = im[u];
-                    double v_re = re[v];
-                    double v_im = im[v];
-                    auto tmp = complex_mul(v_re, v_im, w_re, w_im);
-                    v_re = tmp.first;
-                    v_im = tmp.second;
+                    T u_re = re[u];
+                    T u_im = im[u];
+                    T v_re = re[v];
+                    T v_im = im[v];
+                    complex_mul(v_re, v_im, w_re, w_im, v_re, v_im);
 
-                    // a[i+j] = u + v;
                     re[u] = u_re + v_re;
                     im[u] = u_im + v_im;
 
-                    // a[i+j+len/2] = u - v;
                     re[v] = u_re - v_re;
                     im[v] = u_im - v_im;
 
-                    // std::cout << "[Debug fft_simd seq bef : " 
-                    //     << "len = " << len 
-                    //     << " i = " << i 
-                    //     << " j = " << j
-                    //     << " w_re = " << w_re 
-                    //     << " w_im = " << w_im 
-                    //     << " ]\n";
-                    // w *= wlen;
-                    tmp = complex_mul(w_re, w_im, wlen_re, wlen_im);
-                    w_re = tmp.first;
-                    w_im = tmp.second;
-                    // std::cout << "[Debug fft_simd seq : " 
-                    //     << "len = " << len 
-                    //     << " i = " << i 
-                    //     << " j = " << j
-                    //     << " w_re = " << w_re 
-                    //     << " w_im = " << w_im 
-                    //     << " ]\n";
+                    complex_mul(w_re, w_im, wlen_re, wlen_im, w_re, w_im);
                 }
             }
         }
         else
         {
-            // std::cout << "into simd code len : " << len << '\n';
-
-            double ang = 2 * PI / len * (invert ? -1 : 1);
-            // cd wlen(cos(ang), sin(ang));
-            double cos_ang = cos(ang), sin_ang = sin(ang);
-            // std::cout << "fft simd cos : " << cos_ang << '\t' << "sin : " << sin_ang << "\n";
-            simd<double> wlen_re(cos_ang);
-            simd<double> wlen_im(sin_ang);
+            T ang = 2 * PI / len * (invert ? -1 : 1);
+            T cos_ang = cos(ang), sin_ang = sin(ang);
+            simd_t wlen_re(cos_ang);
+            simd_t wlen_im(sin_ang);
             for (int i = 0; i < n; i += len) 
             {
-                // cd w(1);
-
-                // double w_re = 1;
-                // double w_im = 0;
-                vector<double> w_re_arr(simd<double>::size(), 0), w_im_arr(simd<double>::size(), 0);
+                std::vector<T> w_re_arr(simd_t::size(), 0), w_im_arr(simd_t::size(), 0);
                 w_re_arr[0] = 1;
                 w_im_arr[0] = 0;
                 for (int k = 1; k < w_im_arr.size(); k++)
                 {
-                    auto tmp = complex_mul(w_re_arr[k-1], w_im_arr[k-1],cos_ang, sin_ang);
-                    w_re_arr[k] = tmp.first;
-                    w_im_arr[k] = tmp.second;
+                    complex_mul(w_re_arr[k-1], w_im_arr[k-1],cos_ang, sin_ang, w_re_arr[k], w_im_arr[k]);
                 }
-                // std::cout << "[Debug start warr:";
-                // for (int alpha = 0; alpha < w_im_arr.size(); alpha++)
-                // {
-                //     std::cout << w_re_arr[alpha] << " " << w_im_arr[alpha] << '\n';
-                // }
-                // std::cout << "Debug end warr]\n";
 
-                simd<double> w_re(w_re_arr.data(), vector_aligned);
-                simd<double> w_im(w_im_arr.data(), vector_aligned);
+                simd_t w_re(w_re_arr.data(), sve::experimental::vector_aligned);
+                simd_t w_im(w_im_arr.data(), sve::experimental::vector_aligned);
                 auto w_re_init = w_re;
                 auto w_im_init = w_im;
-                for (int j = 0; j < len / 2; j += simd<double>::size()) 
+                for (int j = 0; j < len / 2; j += simd_t::size()) 
                 {
-                    // cd u = a[i+j], v = a[i+j+len/2] * w;
                     int u = i+j;
                     int v = i+j+len/2;
 
-                    simd<double> u_re(re.data() + u, vector_aligned);
-                    simd<double> u_im(im.data() + u, vector_aligned);
-                    simd<double> v_re(re.data() + v, vector_aligned);
-                    simd<double> v_im(im.data() + v, vector_aligned);
+                    simd_t u_re(re.data() + u, sve::experimental::vector_aligned);
+                    simd_t u_im(im.data() + u, sve::experimental::vector_aligned);
+                    simd_t v_re(re.data() + v, sve::experimental::vector_aligned);
+                    simd_t v_im(im.data() + v, sve::experimental::vector_aligned);
 
-                    auto tmp = complex_mul(v_re, v_im, w_re, w_im);
-                    v_re = tmp.first;
-                    v_im = tmp.second;
+                    complex_mul(v_re, v_im, w_re, w_im, v_re, v_im);
 
-                    // a[i+j] = u + v;
                     auto uv_re = u_re + v_re;
                     auto uv_im = u_im + v_im;
-                    uv_re.copy_to(re.data() + u, vector_aligned);
-                    uv_im.copy_to(im.data() + u, vector_aligned);
+                    uv_re.copy_to(re.data() + u, sve::experimental::vector_aligned);
+                    uv_im.copy_to(im.data() + u, sve::experimental::vector_aligned);
 
-                    // a[i+j+len/2] = u - v;
                     uv_re = u_re - v_re;
                     uv_im = u_im - v_im;
-                    uv_re.copy_to(re.data() + v, vector_aligned);
-                    uv_im.copy_to(im.data() + v, vector_aligned);
+                    uv_re.copy_to(re.data() + v, sve::experimental::vector_aligned);
+                    uv_im.copy_to(im.data() + v, sve::experimental::vector_aligned);
 
-                    // std::cout << "[Debug fft_simd vec bef : " 
-                    //     << "len = " << len 
-                    //     << " i = " << i 
-                    //     << " j = " << j
-                    //     << " w_re = " << w_re 
-                    //     << " w_im = " << w_im 
-                    //     << " ]\n";
-                    // w *= wlen;
-                    // std::cout << "w_re last = " << w_re.last() << "\t" 
-                    //         << "w_im last = " << w_im.last() << '\n';
-                    tmp = complex_mul(w_re_init, w_im_init, 
-                            simd<double>(w_re.last()), simd<double>(w_im.last()));
-                    // std::cout << "w_re last = " << w_re.last() << "\t" 
-                    //         << "w_im last = " << w_im.last() << '\n';
-                    w_re = tmp.first;
-                    w_im = tmp.second;
-                    // std::cout << "[Debug fft_simd vec : " 
-                    //     << "len = " << len 
-                    //     << " i = " << i 
-                    //     << " j = " << j
-                    //     << " w_re = " << w_re 
-                    //     << " w_im = " << w_im 
-                    //     << " ]\n";
-                    // std::cout << "[Debug fft_simd vec : "
-                    //     << "wlen_re = " << wlen_re
-                    //     << "wlen_im = " << wlen_im
-                    //     << "]\n";
-                    tmp = complex_mul(w_re, w_im, 
-                            wlen_re, wlen_im);
-                    w_re = tmp.first;
-                    w_im = tmp.second;
-                    // std::cout << "[Debug fft_simd vec : " 
-                    //     << "len = " << len 
-                    //     << " i = " << i 
-                    //     << " j = " << j
-                    //     << " w_re = " << w_re 
-                    //     << " w_im = " << w_im 
-                    //     << " ]\n";
+                    complex_mul(w_re_init, w_im_init, 
+                            simd_t(w_re.last()), simd_t(w_im.last()), w_re, w_im);
+                    complex_mul(w_re, w_im, 
+                            wlen_re, wlen_im, w_re, w_im);
                 }
             }  
         }
     }
 
     if (invert) {
-        for (double & x : re)
-            x /= n;
-        for (double & x : im)
-            x /= n;
+        if (n < (int)simd_t::size())
+        {
+            for (T & x : re)
+                x /= n;
+            for (T & x : im)
+                x /= n;
+        }
+        else
+        {
+            for (int i = 0; i < n; i += simd_t::size())
+            {
+                simd_t v(re.data() + i, sve::experimental::vector_aligned);
+                v /= n;
+                v.copy_to(re.data() + i, sve::experimental::vector_aligned);
+            }
+
+            for (int i = 0; i < n; i += simd_t::size())
+            {
+                simd_t v(im.data() + i, sve::experimental::vector_aligned);
+                v /= n;
+                v.copy_to(im.data() + i, sve::experimental::vector_aligned);
+            }
+        }
     }
 }
 
-vector<double> gen_signal(double sr)
+template <typename T>
+double test_fft(T freq)
 {
-    double ts = 1.0/sr;
-    vector<double> ret;
-    for (double i = 0; i < 1; i += ts)
-    {
-        ret.push_back(3.0 * sin(2.0 * PI * 2.0 * i));
-    }
-    return ret;
-}
+    using cd = std::complex<T>;
+    auto x = gen_signal(freq);
+    auto x_re = x;
+    auto x_re_simd = x;
+    std::vector<T> x_im(x_re.size(), 0);
+    std::vector<T> x_im_simd(x_re_simd.size(), 0);
+    auto t1 = std::chrono::high_resolution_clock::now();
+        fft(x_re, x_im, false);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = t2 - t1;
 
-// void file_write(std::string file_name, )
-void test_fft(double sr)
-{
-    auto x_re = gen_signal(sr);
-    auto x_re_simd = gen_signal(sr);
-    vector<double> x_im(x_re.size(), 0);
-    vector<double> x_im_simd(x_re_simd.size(), 0);
-    std::cout << "signal equal : " << ((x_re == x_re_simd) && (x_im == x_im_simd)) << '\n';
-
-    // std::cout << "\n Input signal \n\n";
-    // for (int i = 0; i < x_re.size(); i++)
-    // {
-    //     std::cout << x_re[i] << "," << x_im[i] << '\n';
-    //     std::cout << x_re_simd[i] << "," << x_im_simd[i] << '\n';
-    // }
-
-    // std::cout << "\n FFT \n\n";
-    fft(x_re, x_im, false);
+    t1 = std::chrono::high_resolution_clock::now();
     fft_simd(x_re_simd, x_im_simd, false);
-    std::cout << "fft equal : " << ((x_re == x_re_simd) && (x_im == x_im_simd)) << '\n';
-    for (int i = 0; i < x_re.size(); i++)
-    {
-        std::cout << x_re[i] << "," << x_im[i] << '\n';
-        std::cout << x_re_simd[i] << "," << x_im_simd[i] << '\n';
-    }
+    t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff2 = t2 - t1;
+    return diff.count() / diff2.count();
+}
 
-    std::cout << "\n Abs FFT \n\n";
-    for (int i = 0; i < x_re.size(); i++)
+template <typename T>
+void test(int iterations)
+{
+    std::fstream fout("fft.txt");
+    using simd_t = sve::experimental::simd<T>;
+    fout << "type\t\tsimd_len\t\tfrequency\t\tn_samples\t\tspeed_up\n";
+    for (int i = 5; i <= 20; i++)
     {
-        std::cout << abs(cd(x_re[i], x_im[i])) << '\n';
-        std::cout << abs(cd(x_re_simd[i], x_im_simd[i])) << '\n';
-    }
-
-    std::cout << "\n Inverse FFT \n\n";
-    fft(x_re, x_im, true);
-    fft_simd(x_re_simd, x_im_simd, true);
-    for (int i = 0; i < x_re.size(); i++)
-    {
-        std::cout << x_re[i] << "," << x_im[i] << '\n';
-        std::cout << x_re_simd[i] << "," << x_im_simd[i] << '\n';
-    }
-
-    std::cout << "\n Inverse Abs FFT \n\n";
-    for (int i = 0; i < x_re.size(); i++)
-    {
-        std::cout << abs(cd(x_re[i], x_im[i])) << '\n';
-        std::cout << abs(cd(x_re_simd[i], x_im_simd[i])) << '\n';
+        double avg = 0.0;
+        for (int iter = 0; iter < iterations; ++iter)
+        {
+            avg += test_fft<T>(T(std::pow(2, i)));
+        }
+        avg /= iterations;
+        fout << typeid(T).name() << "\t\t"
+                << simd_t::size() << "\t\t"
+                << std::pow(2, i) << "\t\t"
+                << std::pow(2, i) * 16 << "\t\t"
+                << avg << '\n';
     }
 }
 
 int main()
 {
-    std::cout << "simd len : " << simd<double>::size() << '\n';
-    test_fft(512);
+    test<float>(10);
+    test<double>(10);
 }
